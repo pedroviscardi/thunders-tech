42aba5e79863b8e4adafe2afd0b28b699aad90de
Pedro Paulo Orasmo Viscardi <pedro@viscarditecnologia.com.br>
2024-10-28 21:41:03 -0300

    feat: Improvements Dto Documentation

--- src/Thunders.Tecnologia.Application/DTOs/PersonDto.cs
+++ src/Thunders.Tecnologia.Application/DTOs/PersonDto.cs
@@ -2,18 +2,42 @@
 
 namespace Thunders.Tecnologia.Application.DTOs;
 
+/// <summary>
+///     Data transfer object representing a person.
+/// </summary>
 public class PersonDto
 {
-    public Guid Id { get; set; }
+    /// <summary>
+    ///     Gets or sets the unique identifier of the person.
+    /// </summary>
+    public Guid Id { get; set; } = Guid.NewGuid();
 
-    [Required]
-    [StringLength(100)]
+    /// <summary>
+    ///     Gets or sets the name of the person.
+    /// </summary>
+    /// <remarks>
+    ///     This property is required and has a maximum length of 100 characters.
+    /// </remarks>
+    [Required(ErrorMessage = "Name is required.")]
+    [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
     public required string Name { get; set; }
 
-    [Required]
-    [EmailAddress]
+    /// <summary>
+    ///     Gets or sets the email address of the person.
+    /// </summary>
+    /// <remarks>
+    ///     This property is required and must be a valid email address.
+    /// </remarks>
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
     public required string Email { get; set; }
 
-    [Required]
+    /// <summary>
+    ///     Gets or sets the date of birth of the person.
+    /// </summary>
+    /// <remarks>
+    ///     This property is required.
+    /// </remarks>
+    [Required(ErrorMessage = "Date of Birth is required.")]
     public required DateTime DateOfBirth { get; set; }
 }
